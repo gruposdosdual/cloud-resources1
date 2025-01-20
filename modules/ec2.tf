be36e66a78c8c158bf06dcc4e41e8dab60545787
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-01-20 19:22:29 +0100

    Add ec2, rds, securitygroup, ssh key provisioner

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -7,21 +7,84 @@ resource "aws_instance" "ec2_instance" {
     Name       = "ec2-instance"
     Environment = "feature/add-resources"
   }
+}
 
+# Provisioner
+resource "null_resource" "provision_file" {
+  depends_on = [aws_instance.web_server]
+  
+  # Copiar el archivo install.yaml a la instancia remota
+  provisioner "file" {
+    source      = "install.yaml"
+    destination = "/home/ubuntu/install.yaml"
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/my-aws-key.pem")
+      host        = aws_instance.web_server.public_ip
+      timeout     = "5m"
+    }
+  }
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "/home/ubuntu/docker-compose.yml"
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/my-aws-key.pem")
+      host        = aws_instance.web_server.public_ip
+      timeout     = "5m"
+    }
+  }
+  # Ejecutar el playbook de Ansible
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/my-aws-key.pem")
+      host        = aws_instance.web_server.public_ip
+      timeout     = "5m"
+    }
+
+    inline = [
+      "echo 'Starting Ansible setup...'",
+      "sudo apt update",
+      "sudo apt install -y ansible",
+      "ansible-playbook -i 'localhost,' -c local /home/ubuntu/install.yaml"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
   # Opcional: Asocia una clave SSH para acceso remoto
   key_name = aws_key_pair.ssh_keypair.key_name
 }
 
 resource "aws_key_pair" "ssh_keypair" {
   key_name   = "my-aws-key" # Nombre de la clave existente
-  public_key = file("~/.ssh/my-aws-key.pub")
+  public_key = file("~/.ssh/my-aws-key.pem")
+  
 
   lifecycle {
     ignore_changes = [key_name] # Ignora cambios en la clave
   }
 }
 
-
+*/
 
 
 /*
