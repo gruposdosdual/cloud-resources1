279b311e679494cd3717a6f4b3b85ecefea3421f
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-01-20 18:17:06 +0100

    Add vpc

---
+++ modules/vpc.tf
@@ -0,0 +1,17 @@
+module "vpc"{
+    source = "terraform-aws-modules/vpc/aws"
+
+    name = "vpc-jgl"
+    cidr = "10.0.0.0/16"
+
+    azs = ["eu-west-3a","eu-west-3b", "eu-west-3c"]
+    private_subnets = ["10.0.1.0/24", "10.0.2.2/24", "10.0.3.0/24"]
+    public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+    create_igw = true
+    enable_nat_gateway = true
+    single_nat_gateway = false
+
+
+
+}
