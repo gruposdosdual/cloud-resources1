944f498ab8cf4dd33462b1e16bddb6edf46cfe4f
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-01-30 19:44:49 +0100

    Add ECR deployment workflow

--- modulo_ecr_deploy/iam.tf
+++ modulo_ecr_deploy/iam.tf
@@ -14,7 +14,7 @@ resource "aws_iam_role" "github_actions_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:gruposdosdual/cloud-resources1:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:gruposdosdual/cloud-resources1:ref:refs/heads/dev"
           }
         }
       }
@@ -52,3 +52,16 @@ resource "aws_iam_policy_attachment" "github_ecr_policy_attachment" {
   roles      = [aws_iam_role.github_actions_role.name]
 }
 
+resource "aws_iam_openid_connect_provider" "github_oidc" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]  # Thumbprint actual
+}
+
+/*
+resource "aws_iam_role_policy_attachment" "github_actions_role_policy_attachment" {
+    role       = aws_iam_role.github_actions_role.id
+    policy_arn = aws_iam_policy.github_ecr_policy.arn
+}
+
+*/
