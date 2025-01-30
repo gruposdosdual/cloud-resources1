ef72be73323c6beb3f8c1633227e19509a1cfd3c
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-01-30 19:19:36 +0100

    Add ECR deployment workflow

---
+++ modulo_ecr_deploy/iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "MyGitHubActionsRole"
+  description = "Crear rol de Github en AWS"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::2481899437:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:gruposdosdual/cloud-resources1:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "github_ecr_policy" {
+  name        = "GitHubActionsECRPolicy"
+  description = "Permisos mínimos para que GitHub Actions trabaje con AWS ECR"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "github_ecr_policy_attachment" {
+  name       = "GitHubActionsECRPolicyAttachment"
+  policy_arn = aws_iam_policy.github_ecr_policy.arn
+  roles      = [aws_iam_role.github_actions_role.name]
+}
+
