f8e65cce41d3a2cae62fc7db22d943b9aaa23910
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-02-03 18:46:02 +0100

    despliegue final app chat eks aws en github actions de ci-cd

--- chat_app/provider.tf
+++ chat_app/provider.tf
@@ -1,4 +1,13 @@
 provider "aws" {
+  region  = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+/*
+provider "aws" {
   region = var.aws_region
   profile = "248189943700_EKS-alumnos"
 }
+*/
+
--- chat_app/variables.tf
+++ chat_app/variables.tf
@@ -4,6 +4,25 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_session_token" {
+  description = "AWS Session Token"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
 variable "cluster_name" {
   description = "EKS Cluster name"
   type        = string
